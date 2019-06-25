f454ec5a6bd390ea3ef8da48c75f421b011e884b
bigonthepig <bigonthepig@users.noreply.github.com>
2019-06-25 09:05:01 +0100

    25-06-19
    performance improvement

--- Blackboard Quiz Form/Blackboard Quiz Form/ThisDocument.cs
+++ Blackboard Quiz Form/Blackboard Quiz Form/ThisDocument.cs
@@ -21,18 +21,13 @@ namespace Blackboard_Quiz_Form
     }
     public partial class ThisDocument
     {
-        Question lastQuestionOnPreviousPage;
         private List<Question> questionList = new List<Question>();
-        List<List<Question>> listOfQuestionListsByPage = new List<List<Question>>();
         private void ThisDocument_Startup(object sender, System.EventArgs e)
         {
             Question newQuestion = new Question();
             newQuestion.QuestionItem = SelectContentControlsByTag("question")[1];
             newQuestion.QuestionNumber = 1;
             questionList.Add(newQuestion);
-            List<Question> newPageList = new List<Question>();
-            listOfQuestionListsByPage.Add(newPageList);
-            newPageList.Add(newQuestion);
         }
         private void ThisDocument_Shutdown(object sender, System.EventArgs e)
         {
@@ -59,44 +54,20 @@ namespace Blackboard_Quiz_Form
                 Question NewQuestion = new Question();
                 NewQuestion.QuestionItem = NewContentControl;
                 questionList.Add(NewQuestion);
-                int newPageCount = this.Content.get_Information(Microsoft.Office.Interop.Word.WdInformation.wdNumberOfPagesInDocument);
-                foreach (Question q in questionList)
-                {
-                    q.QuestionPosition = q.QuestionItem.Range.Information[Word.WdInformation.wdVerticalPositionRelativeToPage];
-                }
-                if (newPageCount > 1)
-                {
-                    List<Question> previousPageQuestions = questionList.FindAll(x => x.QuestionItem.Range.Information[Word.WdInformation.wdActiveEndPageNumber] == newPageCount - 1);
-                    lastQuestionOnPreviousPage = previousPageQuestions.OrderByDescending(item => item.QuestionPosition).First();
-                    Debug.WriteLine("The last question on the previous page is " + lastQuestionOnPreviousPage.QuestionNumber);
-                }
-                foreach (Question q in questionList)
-                {
-                    if (q.QuestionItem.Range.Information[Word.WdInformation.wdActiveEndPageNumber] > 1)
+                NewQuestion.QuestionPosition = NewQuestion.QuestionItem.Range.Information[Word.WdInformation.wdVerticalPositionRelativeToPage] + NewQuestion.QuestionItem.Range.Information[Word.WdInformation.wdActiveEndPageNumber] * 1000;
+                List<Question> questionsAbove = questionList.FindAll(x => x.QuestionPosition >= NewQuestion.QuestionPosition);
+                if(questionsAbove.Count !=0)
+                { 
+                    foreach(Question q in questionsAbove)
                     {
-                        q.QuestionPosition += lastQuestionOnPreviousPage.QuestionPosition;
+                        q.QuestionPosition = q.QuestionItem.Range.Information[Word.WdInformation.wdVerticalPositionRelativeToPage] + q.QuestionItem.Range.Information[Word.WdInformation.wdActiveEndPageNumber] * 1000;
                     }
                 }
-                IOrderedEnumerable<Question> ordered = questionList.OrderBy(Question => Question.QuestionPosition);
-                List<Question> orderedQuestions = ordered.ToList();
+                List<Question> orderedQuestions = questionList.OrderBy(o => o.QuestionPosition).ToList();
                 foreach (Question q in orderedQuestions)
                 {
-                    if (orderedQuestions.IndexOf(q) > 0)
-                    { 
                         q.QuestionNumber = orderedQuestions.IndexOf(q) + 1;
                         q.QuestionItem.Title = "Question " + q.QuestionNumber;
-                    }
-                    /*
-                if (orderedQuestions.IndexOf(q) > 0)
-                {
-                    List<Question> questionSubList = orderedQuestions.GetRange(0, orderedQuestions.IndexOf(q));
-
-                }
-
-                q.QuestionNumber = orderedQuestions.IndexOf(q) + 1;
-                q.QuestionItem.Title = "Question " + q.QuestionNumber;
-                Debug.WriteLine(q.QuestionItem.Title + "; " + q.QuestionPosition);
-                */
                 }
             }
         }
